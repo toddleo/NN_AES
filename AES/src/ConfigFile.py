84127749e13110e9335e7975dd69009ff37e31d6
Colin Zhang <me@colinzhang.com>
2017-11-15 21:14:20 -0500

    Embedding size 25

--- AES/src/ConfigFile.py
+++ AES/src/ConfigFile.py
@@ -1,13 +1,20 @@
 class Configuration():
     def __init__(self):
+        self.batch_size = 30
+        self.test_batch_size = 10
         self.hidden_size = 50
-        self.input_size = 389
-        self.max_length = 0
+        self.embedding_output = 25
+
         self.dropout = 0.2
         self.output_size = 4
-        self.batch_size = 30
         self.epochs = 100
         self.init_lr = 0.5
+
+        self.max_length = 0
         self.embedding_size = 0
-        self.embedding_output = 50
-        self.test_batch_size = 10
+
+
+
+
+
+
