9c3900ae5938caa63bc9f768419a704c5f1d506a
Rokeer <me@colinzhang.com>
2017-11-28 09:12:52 -0500

    batch size 2

--- AES/src/ConfigFile.py
+++ AES/src/ConfigFile.py
@@ -1,6 +1,6 @@
 class Configuration():
     def __init__(self):
-        self.batch_size = 1
+        self.batch_size = 2
         self.test_batch_size = 1
         self.hidden_size = 100
         self.embedding_output = 50
